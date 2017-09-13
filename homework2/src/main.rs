d060425ffb7c853963de6008b815895ba056d578
David Van <David@DavidVan.net>
2017-09-13 13:55:55 -0700

    Fixed spacing.

--- homework2/src/main.rs
+++ homework2/src/main.rs
@@ -26,14 +26,19 @@ fn main() {
     let mut file = File::open("all-nps-sites.json").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents);
+
     let documents: Corpus = serde_json::from_str(&contents).unwrap();
+
     for (i, document) in documents.documents.iter().enumerate() {
         use std::error::Error;
+
         let file_name = format!("article{}.json", i);
-        let mut file = match  File::create(&file_name) {
+
+        let mut file = match File::create(&file_name) {
             Err(why) => panic!("Couldn't create {}: {}", &file_name, why.description()),
             Ok(file) => file,
         };
+
         let json = serde_json::to_string(&document).unwrap();
         file.write(&json.as_bytes());
     }
